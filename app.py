5ec62fafa3a6d520eea97f64e126f7748a1959fe
Stavitskii <pochta100@mail.ru>
2022-07-06 07:21:00 +0300

    user_table_filled

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+import datetime
+
+import data
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mybase.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(100))
+    role = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text())
+    start_date = db.Column(db.Date())
+    end_date = db.Column(db.Date())
+    address = db.Column(db.String(100))
+    price = db.Column(db.Float)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+db.create_all()
+
+
+for user in data.USERS:
+    db.session.add(User(
+        id=user['id'],
+        first_name=user['first_name'],
+        last_name=user['last_name'],
+        age=user['age'],
+        email=user['email'],
+        role=user['role'],
+        phone=user['phone']
+    ))
+    db.session.commit()
