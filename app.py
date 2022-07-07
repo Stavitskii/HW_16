e3a78f26bec84a7c7695bc22ac6f959f182a9099
Stavitskii <pochta100@mail.ru>
2022-07-07 07:43:31 +0300

    users_and_user_roots_work

--- app.py
+++ app.py
@@ -1,85 +1,34 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import time
-import datetime
+import json
 
-import data
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mybase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+    if request.method == 'GET':
+        res = []
+        for user in User.query.all():
+            res.append(user.to_dict())
+        return jsonify(res)
+        # return json.dumps([user.to_dict() for user in User.query.all()]) -//- то же самое, что предыдущие 4 строки
 
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
-    age = db.Column(db.Integer)
-    email = db.Column(db.String(100))
-    role = db.Column(db.String(100))
-    phone = db.Column(db.String(100))
-
-
-class Order(db.Model):
-    __tablename__ = 'order'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    description = db.Column(db.Text())
-    start_date = db.Column(db.Date())
-    end_date = db.Column(db.Date())
-    address = db.Column(db.String(100))
-    price = db.Column(db.Float)
-    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+@app.route('/users/<int:user_id>', methods=['GET', 'PUT', 'Delete'])
+def one_user(user_id):
+    if request.method == 'GET':
+        try:
+            return jsonify(User.query.get(user_id).to_dict())
+        except:
+            return "User is not found"
 
 
-class Offer(db.Model):
-    __tablename__ = 'offer'
-    id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-db.drop_all()
-db.create_all()
 
 
-for user in data.USERS:
-    db.session.add(User(
-        id=user['id'],
-        first_name=user['first_name'],
-        last_name=user['last_name'],
-        age=user['age'],
-        email=user['email'],
-        role=user['role'],
-        phone=user['phone']
-    ))
-    db.session.commit()
 
-for order in data.ORDERS:
-#    date_obj_start_list = order['start_date'].split("/")
-#    date_obj_start = datetime.date(year=date_obj_start_list[2], month=date_obj_start_list[0],day=date_obj_start_list[1])
-    month_start, day_start, year_start = [int(_) for _ in order['start_date'].split("/")]
-    month_end, day_end, year_end = [int(_) for _ in order['end_date'].split("/")]
-    db.session.add(Order(
-        id=order['id'],
-        name=order['name'] ,
-        description=order['description'] ,
-        start_date=datetime.date(year=year_start, month=month_start,day=day_start),
-        end_date=datetime.date(year=year_end, month=month_end,day=day_end),
-        address=order['address'],
-        price=order['price'],
-        customer_id=order['customer_id'],
-        executor_id=order['executor_id']
-    ))
-    db.session.commit()
-
-db.session.add_all([Offer(id=offer['id'], order_id=offer['order_id'], executor_id=offer['executor_id']) for offer in data.OFFERS])
-#for offer in data.OFFERS:
-#    db.session.add(Offer(
-#        id=offer['id'],
-#        order_id=offer['order_id'],
-#        executor_id=offer['executor_id']
-#    ))
-db.session.commit()
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,43 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(100))
+    role = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age,
+            "email": self.email,
+            "role": self.role,
+            "phone": self.phone
+        }
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text())
+    start_date = db.Column(db.Date())
+    end_date = db.Column(db.Date())
+    address = db.Column(db.String(100))
+    price = db.Column(db.Float)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
